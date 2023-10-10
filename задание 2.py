38cf0769883ee64ab1b4be3b4b86d6e5873c812f
Bui Thanh Dung <buidungitmo2227@gmail.com>
2023-10-10 13:52:51 +0300

    Update task2.py

--- задание 2.py
+++ задание 2.py
@@ -1,9 +1,9 @@
-a=0
+book_counts=0
 with open("books.csv", "r", encoding="cp1251", errors="ignore") as file:
     for line in file:
-        b=[]
-        b=line.split(";")
-        if len(b)>1:
-           if len(b[1])>30:
-               a+=1
+        book=[]
+        book=line.split(";")
+        if len(book)>1:
+           if len(book[1])>30:
+               book_counts+=1
 print ("количество записей, у которых в поле Название строка длиннее 30 символов: ",a)
